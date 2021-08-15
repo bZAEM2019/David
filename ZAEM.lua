0c96b9d87a72400c063d692327c5a6fb80e03827
bZAEM2019 <88965099+bZAEM2019@users.noreply.github.com>
2021-08-15 16:53:27 +0300

    Add files via upload

--- ZAEM.lua
+++ ZAEM.lua
@@ -36,7 +36,6 @@ else
 print('\27[1;31m┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\nلم يتم حفظ ايدي المطور الاساسي ارسله مره اخرى\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉') 
 end
 os.execute('lua ZAEM.lua') 
-end
 end 
 if not DevRio:get(Server.."TokenZAEM") then 
 io.write('\27[1;35m\nالان قم بارسال توكن البوت ↫ ⤈\n\27[0;33;49m') 
