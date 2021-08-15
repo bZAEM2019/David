611e0d77a5314d153399c0a37f811c8beb0ee287
bZAEM2019 <88965099+bZAEM2019@users.noreply.github.com>
2021-08-15 13:18:07 +0300

    Add files via upload

--- ZAEM.lua
+++ ZAEM.lua
@@ -10904,7 +10904,7 @@ if SecondSudo(msg) then
 if text == "تحديث السورس" and SourceCh(msg) or text == "تحديث سورس" and SourceCh(msg) or text == "↫ تحديث السورس ᥀" and SourceCh(msg) then 
 Dev_Rio(msg.chat_id_, msg.id_, 1, '᥀︙جاري تحديث سورس ديفد', 1, 'md') 
 os.execute('rm -rf ZAEM.lua') 
-os.execute('wget https://raw.githubusercontent.com/ZAEMTeam8/ZAEM/master/ZAEM.lua') 
+os.execute('wget https://raw.githubusercontent.com/bZAEM2019/ZAEM/master/ZAEM.lua') 
 dofile('ZAEM.lua') 
 io.popen("rm -rf ../.telegram-cli/*")
 print("\27[31;47m\n          ( تم تحديث السورس )          \n\27[0;34;49m\n") 
@@ -10985,7 +10985,7 @@ end
 send(msg.chat_id_, msg.id_,Files)
 end
 if text == "متجر الملفات" and ChCheck(msg) or text == 'المتجر' and ChCheck(msg) or text == '↫  المتجر ᥀' and ChCheck(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/ZAEMTeam8/ZAEMFiles/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/bZAEM2019/ZAEMFiles/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -11017,7 +11017,7 @@ end
 if text and text:match("^(تعطيل ملف) (.*)(.lua)$") and ChCheck(msg) then
 local FileGet = {string.match(text, "^(تعطيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/ZAEMTeam8/ZAEMFiles/master/ZAEMFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/bZAEM2019/ZAEMFiles/master/ZAEMFiles/"..FileName)
 if Res == 200 then
 os.execute("rm -fr Files/"..FileName)
 send(msg.chat_id_, msg.id_,"\n᥀︙الملف ↫ *"..FileName.."*\n᥀︙تم تعطيله وحذفه من البوت بنجاح") 
@@ -11029,7 +11029,7 @@ end
 if text and text:match("^(تفعيل ملف) (.*)(.lua)$") and ChCheck(msg) then
 local FileGet = {string.match(text, "^(تفعيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/ZAEMTeam8/ZAEMFiles/master/ZAEMFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/bZAEM2019/ZAEMFiles/master/ZAEMFiles/"..FileName)
 if Res == 200 then
 local ChekAuto = io.open("Files/"..FileName,'w+')
 ChekAuto:write(GetJson)
